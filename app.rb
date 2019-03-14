c244de80936320927370d4321236bfca8ece61b6
Cheryl K <cherylkelson@icloud.com>
2019-03-14 17:47:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,11 +17,11 @@ class App < Sinatra::Base
   end
 
 end
-*************
-it "displays the pig latinized phrase upon form submission" do
-  expect(last_response.body).to include("Onceway uponway away imetay andway away eryvay oodgay imetay itway asway erethay asway away oocowmay omingcay ownday alongway ethay oadray andway isthay oocowmay atthay asway omingcay ownday alongway ethay oadray etmay away icenay ittlelay oybay amednay abybay uckootay")
-end
-
-it "displays the pig latinized phrase upon form submission" do
-  expect(last_response.body).to include("eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway ethay ulfGay eamStray andway ehay adhay onegay eightyway ourfay aysday ownay ithoutway akingtay away ishfay")
-end
+# *************
+# it "displays the pig latinized phrase upon form submission" do
+#   expect(last_response.body).to include("Onceway uponway away imetay andway away eryvay oodgay imetay itway asway erethay asway away oocowmay omingcay ownday alongway ethay oadray andway isthay oocowmay atthay asway omingcay ownday alongway ethay oadray etmay away icenay ittlelay oybay amednay abybay uckootay")
+# end
+# 
+# it "displays the pig latinized phrase upon form submission" do
+#   expect(last_response.body).to include("eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway ethay ulfGay eamStray andway ehay adhay onegay eightyway ourfay aysday ownay ithoutway akingtay away ishfay")
+# end
